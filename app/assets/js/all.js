f024b4a7c514f92e7b4b47d6afcc515a4d46763e
Vivian <vivianhuang197@gmail.com>
2022-09-21 22:46:22 +0800

    update index

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,43 @@
 $(function() {
-  console.log('Hello Bootstrap5');
+  
+  $(".menuOpen").click(function(e){
+    e.preventDefault();
+    $(".menuOpen").toggleClass("d-none");
+    $(".menuClose").toggleClass("d-block");
+
+
+  });
+  
+  $(".menuClose").click(function(e){
+    e.preventDefault();
+    $(".menuOpen").toggleClass("d-none");
+    $(".menuClose").toggleClass("d-block");
+
+
+  });
+
+
 });
+
+//artist 輪播
+
+
+var swiper = new Swiper(".artistSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+
+    breakpoints: {
+      1200: {
+        centeredSlides:true,
+        slidesPerView: 3,
+        spaceBetween: 24,
+      }
+    },
+    loop: true,
+    grabCursor: true,
+    pagination: {
+    el: ".swiper-pagination",
+          clickable: true,
+    },
+});
+ 
